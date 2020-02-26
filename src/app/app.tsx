c7740a53a9a943f05623b9b944de895e7697c59d
Tianlikai <1024998526@qq.com>
2020-02-26 18:48:54 +0800

    refactor: 删除app && module目录

--- src/app/app.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.scss";
-
-const PREFIX = "App";
-
-const App = () => <div className={`${PREFIX}`}>this is react app</div>;
-
-export default App;
--- src/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "lib-flexible/flexible";
-
-import App from "./app";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
