fcf36e3d2c30603085f97f6ea2bfaef2d1089714
Tianlikai <1024998526@qq.com>
2020-03-08 16:31:25 +0800

    build: eCharts 单独打包

--- webpack/webpack.dll.js
+++ webpack/webpack.dll.js
@@ -5,6 +5,7 @@ module.exports = {
   mode: "production",
   entry: {
     react: ["react", "react-dom", "react-router", "react-router-dom"],
+    eCharts: ["echarts"],
   },
   output: {
     path: path.resolve(__dirname, "../dll"),
